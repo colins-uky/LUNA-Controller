d619e1c5bec7e1ec4d7a28bcd0791c58b57b32f7
colins-uky <colin.schuh@uky.edu>
2023-03-09 22:48:08 -0500

    Added forward, reverse, turnRight, and turnLeft functions.

--- Webpage/index.js
+++ Webpage/index.js
@@ -1,100 +1,63 @@
-var select = document.getElementById("motorSelect");
-var motor = select.options[select.selectedIndex].value;
+
 var message_out = 'M1000100010001000';
 var m1='0000';
 var m2='0000';
 var m3='0000';
 var m4='0000';
 
-function val()
-{
-	select = document.getElementById("motorSelect");
-	motor = (select.options[select.selectedIndex].value);
-
-	imgPath = "../Images/car-icon-motor" + motor + ".png";
-	document.getElementById("car-icon").src = imgPath;
-}
 
 /* TODO add websocket communication stuff for each of the buttons*/
 function forward() {
-	if(motor=="1"){
-		m1='1100';
-		m2='0000';
-		m3='0000';
-		m4='0000';
-	}
-	else if(motor=="2"){
-		m1='0000';
-		m2='1100';
-		m3='0000';
-		m4='0000';
-	}
-	else if(motor=="3"){
-		m1='0000';
-		m2='0000';
-		m3='0100';
-		m4='0000';
-	}
-	else if(motor=="4"){
-		m1='0000';
-		m2='0000';
-		m3='0000';
-		m4='0100';
-	}
+	m1='1100';
+	m2='1100';
+	m3='0100';
+	m4='0100';
+
+	document.getElementById('current-action').textContent = "forward()";
 }
+
 function stop() {
-	if(motor=="1"){
-		m1='2000';
-		m2='0000';
-		m3='0000';
-		m4='0000';
-	}
-	else if(motor=="2"){
-		m1='0000';
-		m2='2000';
-		m3='0000';
-		m4='0000';
-	}
-	else if(motor=="3"){
-		m1='0000';
-		m2='0000';
-		m3='2000';
-		m4='0000';
-	}
-	else if(motor=="4"){
-		m1='0000';
-		m2='0000';
-		m3='0000';
-		m4='2000';
-	}
-	
+	m1='2000';
+	m2='2000';
+	m3='2000';
+	m4='2000';
+	document.getElementById('current-action').textContent = "stop()";
 }
+
 function reverse(){
-	if(motor=="1"){
-		m1='0100';
-		m2='0000';
-		m3='0000';
-		m4='0000';
-	}
-	else if(motor=="2"){
-		m1='0000';
-		m2='0100';
-		m3='0000';
-		m4='0000';
-	}
-	else if(motor=="3"){
-		m1='0000';
-		m2='0000';
-		m3='1100';
-		m4='0000';
-	}
-	else if(motor=="4"){
-		m1='0000';
-		m2='0000';
-		m3='0000';
-		m4='1100';
-	}
+	m1='0100';
+	m2='0100';
+	m3='1100';
+	m4='1100';
+	document.getElementById('current-action').textContent = "reverse()";
 }
+
+function turnRight() {
+    m1='1100';
+    m2='0100';
+    m3='0100';
+    m4='1100';
+    document.getElementById('current-action').textContent = "turnRight()";
+}
+
+function turnLeft() {
+    m1='0100';
+    m2='1100';
+    m3='1100';
+    m4='0100';
+    document.getElementById('current-action').textContent = "turnLeft()";
+}
+
+
+
+
+
+
+
+
+
+
+
 
 function updateMsg(){
 		console.log(motor);
@@ -162,6 +125,7 @@ function removeStyleButton(element) {
 window.addEventListener("gamepadconnected", event => {
     console.log('Gamepad Connected...');
     console.log(event.gamepad);
+	window.requestAnimationFrame(update);
 });
 
 window.addEventListener("gamepaddisconnected", event => {
@@ -170,29 +134,19 @@ window.addEventListener("gamepaddisconnected", event => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-function update() {
-    const gamepads = navigator.getGamepads();
+function getGamepadState() {
+	const gamepads = navigator.getGamepads();
     if (gamepads[0]) {
         const gamepadState = {
             id: gamepads[0].id,
-            axes: [
-                gamepads[0].axes[0].toFixed(3),
-                gamepads[0].axes[1].toFixed(3),
-                gamepads[0].axes[2].toFixed(3),
-                gamepads[0].axes[3].toFixed(3),
-            ],
+            axes: {
+                leftX: gamepads[0].axes[0].toFixed(3),
+                leftY: gamepads[0].axes[1].toFixed(3),
+                rightX: gamepads[0].axes[2].toFixed(3),
+                rightY: gamepads[0].axes[3].toFixed(3),
+			},
 
+			/*
             buttons: [{ 
                 button0: gamepads[0].buttons[0].pressed,
                 button1: gamepads[0].buttons[1].pressed,
@@ -212,18 +166,66 @@ function update() {
                 button15: gamepads[0].buttons[15].pressed,
                 button16: gamepads[0].buttons[16].pressed,
                 button17: gamepads[0].buttons[17].pressed,
-            }]
+            }],	*/
+
+			usefulButtons: {
+				X: gamepads[0].buttons[0].pressed,
+                Circle: gamepads[0].buttons[1].pressed,
+                Square: gamepads[0].buttons[2].pressed,
+                Triangle: gamepads[0].buttons[3].pressed,
+				dpadUp: gamepads[0].buttons[12].pressed,
+                dpadDown: gamepads[0].buttons[13].pressed,
+                dpadLeft: gamepads[0].buttons[14].pressed,
+                dpadRight: gamepads[0].buttons[15].pressed,
+				L1: gamepads[0].buttons[4].pressed,
+				L2: gamepads[0].buttons[6].pressed,
+                R1: gamepads[0].buttons[5].pressed,
+                R2: gamepads[0].buttons[7].pressed,
+				share: gamepads[0].buttons[8].pressed,
+                options: gamepads[0].buttons[9].pressed,
+                touchPad: gamepads[0].buttons[17].pressed,
+			}
         };
 
-        console.log(gamepadState);
-        document.getElementById('gamepad-text').textContent = JSON.stringify(gamepadState);
+		return gamepadState;
     }
+	return null;
+}
 
+
+
+function driverControl(gamepadState) {
+	if (gamepadState) {
+		if (gamepadState.axes.rightY < -0.7 || gamepadState.usefulButtons.dpadUp) {
+			forward();
+		}
+        else if (gamepadState.axes.rightY > 0.7 || gamepadState.usefulButtons.dpadDown) {
+            reverse();
+        }
+        else if (gamepadState.axes.rightX > 0.7 || gamepadState.usefulButtons.dpadRight) {
+            turnRight();
+        }
+        else if (gamepadState.axes.rightX < -0.7 || gamepadState.usefulButtons.dpadLeft) {
+            turnLeft();
+        }
+		else {
+			document.getElementById('current-action').textContent = "no-op";
+		}
+	}
+}
+
+
+
+function update() {
+    let gamepadState = getGamepadState();
+	document.getElementById('gamepad-text').textContent = JSON.stringify(gamepadState);
+
+
+	driverControl(gamepadState);
 
     
     window.requestAnimationFrame(update);
 }
 
-window.requestAnimationFrame(update);
 
 
